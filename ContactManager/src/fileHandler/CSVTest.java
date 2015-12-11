607d82e9b155b640c523580b47aa60abc7f192aa
AARONT59570 <athompson.59570@farnborough.ac.uk>
2015-12-11 01:30:54 +0000

    Interfaces Update
    12/11/2015

--- ContactManager/src/fileHandler/CSVTest.java
+++ ContactManager/src/fileHandler/CSVTest.java
@@ -1,5 +1,6 @@
 package fileHandler;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,13 +9,13 @@ public class CSVTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CSVHandler csv = new CSVHandler();
-		List list = new ArrayList<String[]>();
+		ArrayList<String[]> list = new ArrayList<String[]>();
 		String[] str = {"test1", "test2", "test3", "test4"};
 		list.add(str);
 		list.add(str);
 		list.add(str);
 		list.add(str);
-		csv.WriteFile("c:\\Users\\CompUser\\Documents\\records2.csv", "1,2,3,4", (ArrayList<String[]>)list, true);
+		csv.WriteFile(System.getProperty("user.home") + File.separatorChar + "Documents" + File.separatorChar + "records2.csv", "1,2,3,4", list, true);
 	}
 
 }
